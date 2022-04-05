b0f0abf2dabfedd5fa427781903f4c19c3435aad
LukSt92 <stach.stachu@gmail.com>
2022-04-05 19:33:29 +0200

    Add project files.

---
+++ PracaDomowa02Tydzień04/FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracaDomowa02Tydzień04
+{
+    class FizzBuzz
+    {
+        public void Game()
+        {
+            try
+            { 
+            Console.WriteLine("Witaj w prostej grze o nazwie FizzBuzz, proszę podaj swoją liczbę:");
+            var number = int.Parse(Console.ReadLine());
+           
+                if ((number % 3) == 0 && (number % 5) != 0)
+                    Console.WriteLine("Fizz!");
+                else if ((number % 5) == 0 && (number % 3) != 0)
+                    Console.WriteLine("Buzz!");
+                else if ((number % 3) == 0 && (number % 5) == 0)
+                    Console.WriteLine("FizzBuzz!");
+                else
+                    Console.WriteLine(number);
+            } 
+            catch (Exception)
+            {
+                Console.WriteLine("Podałeś niepoprawne dane!");
+            }
+        }
+
+    }
+}
---
+++ PracaDomowa02Tydzień04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracaDomowa02Tydzień04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fizzbuzz = new FizzBuzz();
+            fizzbuzz.Game();
+        }
+
+    }
+}
